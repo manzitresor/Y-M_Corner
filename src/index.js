724920a97bed5d43831c9a0a6700fa40f751f4aa
yemidada <yemidada@gmail.com>
2023-06-28 10:09:24 +0100

    add index.html and bootscrap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import './styles.css';
 import * as data from './modules/data.js';
 
